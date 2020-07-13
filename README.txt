0df32abefa340d56b102b864e05166cb6b9df901
Mikey Liow Gian Hao <glio0001.student.monash.edu>
2020-07-13 16:45:25 +0800

    adding documentation

---
+++ README.txt
@@ -0,0 +1,12 @@
+Mikey Hao
+github.com/mikeyhao
+
+Simple Sudoku Solving Algorithm
+
+    It reads a .txt file containing 9x9 numeric elements and outputs a solution or none.
+    The algorithm uses recursion and backtracking to ensure there will be an answer whenever possible.
+    The algorithm only works on 9x9 (for now)
+
+Future additions(?)
+
+    Use computer vision to read a sudoku from an image and output the answer from it
--- Sudoku.py
+++ Sudoku.py
@@ -3,6 +3,11 @@
 class Sudoku:
 
     def __init__(self, txt):
+        """
+        Reads a .txt file and creates a 2D-matrix that represents
+        a 9x9 sudoku board
+        :param txt: A .txt file representing a sudoku board
+        """
         f = open("ex/"+txt, "r")
         text = f.read()
         text = text.split("\n")
@@ -15,6 +20,9 @@ class Sudoku:
         self.blanks_cr8()
 
     def blanks_cr8(self):
+        """
+        Creates an array of the empty sudoku cells to be filled in
+        """
         self.blanks = []
         for y in range(len(self.matrix)):
             for x in range(len(self.matrix[y])):
@@ -22,6 +30,11 @@ class Sudoku:
                     self.blanks.append((y,x))
 
     def grid(self, y, x):
+        """
+        :param y: position Y
+        :param x: position X
+        :return: An array representing the grid the (y,x) cell exists in
+        """
         ret = []
         py, px = (y//3)*3, (x//3)*3
         for i in range(py, py+3):
@@ -30,18 +43,36 @@ class Sudoku:
         return ret
 
     def row(self, y, x):
+        """
+        :param y: position Y
+        :param x: position X
+        :return: An array representing the row the (y,x) cell exists in
+        """
         ret = []
         for i in self.matrix[y]:
             ret.append(i)
         return ret
 
     def col(self, y, x):
+        """
+        :param y: position Y
+        :param x: position X
+        :return: An array representing the column the (y,x) cell exists in
+        """
         ret = []
         for i in range(len(self.matrix)):
             ret.append(self.matrix[i][x])
         return ret
 
     def is_possible(self, num, y, x):
+        """
+        Finds whether num is valid at position (y,x). If duplicates
+        exists in the grid, row or column, it is not valid.
+        :param num: number to be checked for repetition
+        :param y: position Y
+        :param x: position X
+        :return: True/False depending on validity
+        """
         arr = [self.grid(y,x), self.row(y,x), self.col(y,x)]
         for ar in arr:
             for a in ar:
@@ -50,6 +81,11 @@ class Sudoku:
         return True
 
     def solve(self):
+        """
+        Call me to solve sudoku! Prints either the answer to the
+        puzzle or indication that solution does not exist
+        :return: None
+        """
         res = self.solve_aux(0)
         if res:
             print(str(self))
@@ -57,6 +93,11 @@ class Sudoku:
             print("There are no solutions to this puzzle!")
 
     def solve_aux(self, idx):
+        """
+        Solves the sudoku based on recursion and backtracking algorithm
+        :param idx: position of current empty cell
+        :return: True if can continue, False if no solution is valid (backtracks)
+        """
         if idx >= len(self.blanks):
             return True
         y,x = self.blanks[idx]
@@ -84,7 +125,11 @@ class Sudoku:
                 ret_string += "\n"
         return ret_string
 
+
 def testr():
+    """
+    Runs the test cases in the ex folder
+    """
     for i in range(5):
         sdo = "sdo_"+str(i)+".txt"
         sol = "sdo_"+str(i)+"_sol.txt"
@@ -95,8 +140,10 @@ def testr():
             assert False
     print("passed everything!")
 
+
 if __name__ == "__main__":
 
     # sd = Sudoku("sdo_4.txt")
     # sd.solve()
-    testr()
+    # testr()
+    pass
